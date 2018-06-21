d713d94e44b31f4d38f7f066dd56d8d7ea68a57c
Hulk <hulkyang@taipay.com>
2018-06-21 09:15:12 +0800

    [新增] cursor view animation, 箭頭擺動根據輪盤的角度速率慢下來

--- app/src/main/java/com/ryan/luckywheel/MainActivity.java
+++ app/src/main/java/com/ryan/luckywheel/MainActivity.java
@@ -1,8 +1,16 @@
 package com.ryan.luckywheel;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -14,7 +22,14 @@ import rubikstudio.library.model.LuckyItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 一回合 5 秒
+    // 上一次的總角度
+    float preAngle = 0;
+
+    ImageView cursorView;
+    ObjectAnimator animCursor;
+    ValueAnimator animCursorSlowDown;
+
+    // 一回合 多少 秒
     private static final int SECOND_OF_ROUND = 10;
     // 幸運輪盤有幾格
     private static final int GRID_OF_LUCKY_WHEEL = 8;
@@ -27,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        final LuckyWheelView luckyWheelView = (LuckyWheelView) findViewById(R.id.luckyWheel);
+        final LuckyWheelView luckyWheelView = findViewById(R.id.luckyWheel);
+        cursorView = findViewById(R.id.imageView);
+
 
         for ( int i = 0; i < GRID_OF_LUCKY_WHEEL; i++ ) {
             LuckyItem luckyItem = new LuckyItem();
@@ -41,19 +58,18 @@ public class MainActivity extends AppCompatActivity {
         luckyWheelView.setPieBackgroundImage(R.drawable.wheel3);
 //        luckyWheelView.setLuckyWheelCursorImage(R.drawable.ic_cursor);
 
-        /*luckyWheelView.setLuckyWheelBackgrouldColor(0xff0000ff);
-        luckyWheelView.setLuckyWheelTextColor(0xffcc0000);
-        luckyWheelView.setLuckyWheelCenterImage(getResources().getDrawable(R.drawable.icon));
-        luckyWheelView.setLuckyWheelCursorImage(R.drawable.ic_cursor);*/
-
-
+        // Play Click listener
         findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int index = getRandomIndex();
                 luckyWheelView.startLuckyWheelWithTargetIndex(index);
+
+                // cursor動畫
+                startCursorAnimation(index);
             }
         });
+
 
         luckyWheelView.setLuckyRoundItemSelectedListener(new LuckyWheelView.LuckyRoundItemSelectedListener() {
             @Override
@@ -71,5 +87,64 @@ public class MainActivity extends AppCompatActivity {
     private int getRandomRound() {
         Random rand = new Random();
         return rand.nextInt(10) + 15;
+    }
+
+    /**
+     * @return
+     */
+    private float getAngleOfIndexTarget(int index) {
+        return (360f / 8) * index;
+    }
+
+    private void startCursorAnimation(int targetIndex) {
+        //小動畫
+        // 200ms
+        animCursor = ObjectAnimator.ofFloat(cursorView, "rotation", 0f, -30f, 0f);
+        animCursor.setDuration(200);
+        animCursor.setInterpolator(new AccelerateInterpolator());
+        animCursor.setRepeatCount(0);
+        animCursor.setRepeatMode(ValueAnimator.RESTART);
+
+        // 設定cursor旋轉的中心點
+        cursorView.setPivotX(0f);
+        cursorView.setPivotY(cursorView.getWidth()/2);
+
+        // 算角度，要轉幾圈（360度 * 圈數 - 目標角度）跟輪盤轉的角度一樣
+        float targetAngle = 360f * SECOND_OF_ROUND - getAngleOfIndexTarget(targetIndex) ;
+
+        // slow down
+        animCursorSlowDown = ValueAnimator.ofFloat(0, targetAngle);
+        // 10秒 1000=1秒
+        animCursorSlowDown.setDuration(SECOND_OF_ROUND * 1000);
+        animCursorSlowDown.setInterpolator(new DecelerateInterpolator());
+        animCursorSlowDown.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(final ValueAnimator animation) {
+                // 目前的角度 + 一開始的偏移角度22.5
+                float currentAngle = (float) animation.getAnimatedValue() + ((360f / GRID_OF_LUCKY_WHEEL) / 2);
+
+                // 這次轉的角度
+                float  angleOfThisTurn = currentAngle - preAngle;
+                // 是否執行動畫
+                int isStartAnimation = (int) (angleOfThisTurn / (360f / GRID_OF_LUCKY_WHEEL));
+
+                if ( isStartAnimation > 0 && !animCursor.isRunning() ) {
+                    Log.e("animCursor", "start()");
+                    preAngle += isStartAnimation * (360f / GRID_OF_LUCKY_WHEEL);
+                    animCursor.start();
+                }
+            }
+        });
+        animCursorSlowDown.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                preAngle = 0;
+            }
+        });
+
+        if ( !animCursorSlowDown.isStarted() ) {
+            animCursorSlowDown.start();
+        }
+
     }
 }
